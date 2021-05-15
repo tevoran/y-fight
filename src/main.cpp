2846a71c61020f984a000a6cc04b40679e0a6245
tevoran <j.pfeiffer93@gmx.de>
2021-05-15 11:44:13 +0200

    a new player sprite

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	yf::game game("Y-FIGHT");
 	yf::camera camera(0.2, game.resx, game.resy, (int)(0.046*(float)game.resy));
 	yf::world world(&game, &camera, "../assets/tileset.png");
-	yf::object player(&game, &camera, "../assets/player.png", 16, 16, (int)(0.026*(float)game.resx), (int)(0.046*(float)game.resy));
+	yf::object player(&game, &camera, "../assets/player.png", 16, 16, (int)(0.052*(float)game.resx), (int)(0.096*(float)game.resy));
 	yf::object cursor(&game, &camera, "../assets/cursor.png", 16, 16, (int)(0.016*(float)game.resx), (int)(0.029*(float)game.resy));
 	player.x=400;
 	player.y=800;
